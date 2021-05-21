c6e244c46a08826bd969733e16fa352d8a5f9e00
Serkin <sdima96@gmail.com>
2021-05-21 20:33:55 +0300

    Fix

--- CreateShellScript.py
+++ CreateShellScript.py
@@ -9,6 +9,7 @@ def main():
     for band_name in CreateCSVs.BANDS:
         line = "dsbulk load -url " + config.KEY_SPACE + "." + band_name + ".csv -k " + config.KEY_SPACE + " -t " + band_name
         data.append(line)
+    f_disc.writelines(data)
     f_disc.close()
 
 
