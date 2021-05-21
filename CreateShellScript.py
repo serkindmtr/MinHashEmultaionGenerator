a55320daf4e7d73b44519e9fe97adf56d9a878db
Serkin <sdima96@gmail.com>
2021-05-21 20:36:01 +0300

    Fix

--- CreateShellScript.py
+++ CreateShellScript.py
@@ -4,10 +4,10 @@ import config
 
 def main():
     f_disc = open('dsbulk_script', 'w+')
-    f_disc.writelines(['#!/bin/bash'])
+    f_disc.writelines(['#!/bin/bash\n'])
     data = []
     for band_name in CreateCSVs.BANDS:
-        line = "dsbulk load -url " + config.KEY_SPACE + "." + band_name + ".csv -k " + config.KEY_SPACE + " -t " + band_name
+        line = "dsbulk load -url " + config.KEY_SPACE + "." + band_name + ".csv -k " + config.KEY_SPACE + " -t " + band_name + "\n"
         data.append(line)
     f_disc.writelines(data)
     f_disc.close()
